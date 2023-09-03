703727079eb835dcabe36b9d2622ddea174ba571
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-09-03 09:41:12 +0900

    ✨ object state instead of multiple smaller ones

---
+++ src/app/objectStateMultiple/bad/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import React, { useState, ChangeEvent } from "react";
+
+export default function Page() {
+  // bad
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  // ...
+
+  const handleChange = () => {
+    // ...
+  };
+
+  return (
+    <form className="p-4">
+      <div className="mb-6">
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your email
+        </label>
+        <input
+          onChange={handleChange}
+          type="email"
+          name="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@flowbite.com"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your password
+        </label>
+        <input
+          onChange={handleChange}
+          type="password"
+          name="password"
+          id="password"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="firstName"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your first name
+        </label>
+        <input
+          onChange={handleChange}
+          type="firstName"
+          name="firstName"
+          id="firstName"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="lastName"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your last name
+        </label>
+        <input
+          onChange={handleChange}
+          type="lastName"
+          name="lastName"
+          id="lastName"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          htmlFor="address"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your address
+        </label>
+        <input
+          onChange={handleChange}
+          type="address"
+          name="address"
+          id="address"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
